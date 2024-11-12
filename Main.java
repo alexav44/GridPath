1466d6b32d40ecd76a4ae11e297309f984217dcd
alexav44 <alexav44@nycstudents.net>
2024-11-12 17:22:38 +0000

    added sumPath test

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main{
             {18,22,30,25,6},
         };
         GridPath one = new GridPath(nums);
+        System.out.println(one);
         System.out.println(one.getNextLoc(0, 0));
         System.out.println(one.getNextLoc(1, 3));
         System.out.println(one.getNextLoc(2, 4));
@@ -20,6 +21,7 @@ public class Main{
         };
         GridPath two = new GridPath(secondGrid);
         System.out.println(two);
-
+        System.out.println(two.sumPath(1, 1));
     }
+    
 }
